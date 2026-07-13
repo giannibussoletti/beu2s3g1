e590c3ddb39be2d9fada0652ee1d92b3a58a43c1
Gianni Bussoletti <giannibussoletti@gmail.com>
2026-07-13 14:02:57 +0200

    end lesson update

--- src/main/java/gianni_bussoletti/beu2s3g1/controllers/AuthController.java
+++ src/main/java/gianni_bussoletti/beu2s3g1/controllers/AuthController.java
@@ -5,6 +5,7 @@ import gianni_bussoletti.beu2s3g1.payloads.LoginDTO;
 import gianni_bussoletti.beu2s3g1.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public AuthResponseDTO login(LoginDTO body) {
+    public AuthResponseDTO login(@RequestBody LoginDTO body) {
         String loginToken = this.authService.checkCredentialsAndGeneratorToken(body);
         return new AuthResponseDTO(loginToken);
     }
--- src/main/java/gianni_bussoletti/beu2s3g1/security/TokenFilter.java
+++ src/main/java/gianni_bussoletti/beu2s3g1/security/TokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -25,16 +26,17 @@ public class TokenFilter extends OncePerRequestFilter {
         // 1. Verifichiamo che ci sia la Authorization nell'header e controllare che sia nel formato corretto "Bearer token"
         // 1.1 SE Auth Header è sbagliato in qualsiasi forma --> errore
         String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || authHeader.startsWith("Bearer "))
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            System.out.println(authHeader);
             throw new UnauthorizedException("Inserire il token nell'Authorization payload nel formato corretto 'Bearer '");
-
+        }
 
         //2. Estraiamo il token dall'header
         // Il metodo replace sostituisce una parte della stringa che gli andiamo a chiedere.
         String accessToken = authHeader.replace("Bearer ", "");
-
+        System.out.println(accessToken);
         //3. Verifichiamo che il token sia OK
-        
+
 
         //3.1 Controlliamo se non sia mal formato
         //3.2 Che non sia scaduto
@@ -42,5 +44,15 @@ public class TokenFilter extends OncePerRequestFilter {
         // Se il token è OK --> andiamo avanti, ad un prossimo filtro, od ad un controller sa siamo gli ultimi filtri
         filterChain.doFilter(request, response); // <-- Questo permette di andare avanti con l'applicazione.
         // Se il token ha problemi --> Errore, rifai il login.
+    }
+
+    @Override
+    // Tramite questo metodo possiamo specificare dei criteri per i quali il filtro non debba essere utilizzato.
+    //Ad esempio in un login
+    //O anche in fase di registrazione
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+//        return request.getServletPath().equals("/auth/login");
+        //Tutti gli endpoint dentro AuthController non avranno la richiesta del token
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
     }
 }
